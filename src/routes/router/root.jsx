5f46cf31d0d449d9ff8cc1791f3f96c5c2313c95
Deymer Ospina <deymer.ospina@gmail.com>
2024-12-30 16:18:29 -0500

    Renaming and reordering folders

--- src/routes/router/root.jsx
+++ src/routes/router/root.jsx
@@ -1,24 +1,24 @@
-import { Route, Routes } from "react-router-dom"
-import Forgot from '../forgot/forgot.jsx'
+import { Route, Routes } from "react-router-dom";
+import Forgot from "../forgot/forgot.jsx";
 
-import Club1 from '../club_form_1/club_form_1.jsx'
-import Club2 from '../club_form_2/club_form_2.jsx'
-import Club3 from '../club_form_3/club_form_3.jsx'
-import Club4 from '../club_form_4/club_form_4.jsx'
+import Club1 from "../club_register/club_register_1.jsx";
+import Club2 from "../club_register/club_register_2.jsx";
+import Club3 from "../club_register/club_register_3.jsx";
+import Club4 from "../club_register/club_register_4.jsx";
 
-import Coach1 from '../coach-form-1/coach-form-1.jsx'
-import Coach2 from '../coach-form-2/coach-form-2.jsx'
+import Coach1 from "../coach_register/coach_register_1.jsx";
+import Coach2 from "../coach_register/coach_register_2.jsx";
 
-import Sport1 from '../sport-form-1/sport-form-1.jsx'
-import Sport2 from '../sport-form-2/sport-form-2.jsx'
-import Sport3 from '../sport-form-3/sport-form-3.jsx'
+import Athlete1 from "../athletes_register/athletes_register_1.jsx";
+import Athlete2 from "../athletes_register/athletes_register_2.jsx";
+import Athlete3 from "../athletes_register/athletes_register_3.jsx";
 
-import Login from '../login/login.jsx'
-import Register from '../register/register.jsx'
-import Success from '../log-success/log-success.jsx'
+import Login from "../login/login.jsx";
+import Register from "../register/register.jsx";
+import Success from "../success_login/success_login.jsx";
 
-import ErrorPage from './error.jsx'
-import Home from "../home/home.jsx"
+import ErrorPage from "./error.jsx";
+import Home from "../home/home.jsx";
 
 export default function Router() {
   return (
@@ -26,22 +26,22 @@ export default function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/forgot" element={<Forgot />} />
 
-      <Route path="/club1" element={<Club1 />} />
-      <Route path="/club2" element={<Club2 />} />
-      <Route path="/club3" element={<Club3 />} />
-      <Route path="/club4" element={<Club4 />} />
+      <Route path="/club-form/step-1" element={<Club1 />} />
+      <Route path="/club-form/step-2" element={<Club2 />} />
+      <Route path="/club-form/step-3" element={<Club3 />} />
+      <Route path="/club-form/step-4" element={<Club4 />} />
 
-      <Route path="/coach1" element={<Coach1 />} />
-      <Route path="/coach2" element={<Coach2 />} />
+      <Route path="/coach-form/step-1" element={<Coach1 />} />
+      <Route path="/coach-form/step-2" element={<Coach2 />} />
 
-      <Route path="/sport1" element={<Sport1 />} />
-      <Route path="/sport2" element={<Sport2 />} />
-      <Route path="/sport3" element={<Sport3 />} />
+      <Route path="/athlete-form/step-1" element={<Athlete1 />} />
+      <Route path="/athlete-form/step-2" element={<Athlete2 />} />
+      <Route path="/athlete-form/step-3" element={<Athlete3 />} />
 
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/success" element={<Success />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
-  )
+  );
 }
