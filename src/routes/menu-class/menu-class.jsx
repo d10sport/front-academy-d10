9ce1e1fe204a440cc0c905be33482bbf776a9a65
Deymer Ospina <deymer.ospina@gmail.com>
2025-03-03 06:08:31 -0500

    menu-class prepared for sending data to the modal

--- src/routes/menu-class/menu-class.jsx
+++ src/routes/menu-class/menu-class.jsx
@@ -1,8 +1,9 @@
+// import Example from "../../assets/img/example-img.png";
 import AddClass from "../../components/admin-class/add-class/add-class.jsx";
 import EditClass from "../../components/admin-class/edit-class/edit-class.jsx";
+import DeleteCourse from "../../components/admin-class/delete-class/delete-class.jsx";
 import { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
-// import Example from "../../assets/img/example-img.png";
 import AppContext from "@context/app/app-context";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -11,24 +12,16 @@ import "./menu-class.css";
 export default function MenuClass() {
   const [modalIsOpenOne, setModalIsOpenOne] = useState(false);
   const [modalIsOpenTwo, setModalIsOpenTwo] = useState(false);
+  const [selectedClass, setSelectedClass] = useState(null);
+  const [modalIsOpenThree, setModalIsOpenThree] = useState(false);
+  const [selectedClassId, setSelectedClassId] = useState(null);
 
-    const { idCourse } = useParams();
-    const context = useContext(AppContext);
-    const urlApi = context.urlApi;
-    const apiKey = context.apiKey;
-  
-    const [classes, setClasses] = useState([]);
+  const { idCourse } = useParams();
+  const context = useContext(AppContext);
+  const urlApi = context.urlApi;
+  const apiKey = context.apiKey;
 
-  // const courses = [
-  //   {
-  //     title: "Introduction",
-  //     description: "Getting started with the course",
-  //   },
-  //   {
-  //     title: "Core Concepts",
-  //     description: "Understanding the fundamentals",
-  //   },
-  // ];
+  const [classes, setClasses] = useState([]);
 
   async function getClassMenu() {
     try {
@@ -76,11 +69,24 @@ export default function MenuClass() {
                 <p className="text__menu-class">{cls.class_description}</p>
               </div>
               <div className="cntr-btn__menu-class">
-                <button
-                  onClick={() => setModalIsOpenTwo(true)}
+              <button
+                  onClick={() => {
+                    setSelectedClass(cls);
+                    setModalIsOpenTwo(true);
+                  }}
                   className="btn-edit__menu-class"
                 >
                   Edit
+                </button>
+
+                <button
+                  onClick={() => {
+                    setSelectedClassId(cls.class_id);
+                    setModalIsOpenThree(true);
+                  }}
+                  className="btn-delete__menu-course"
+                >
+                  Eliminar
                 </button>
               </div>
             </li>
@@ -91,12 +97,21 @@ export default function MenuClass() {
       <AddClass
         isOpen={modalIsOpenOne}
         onClose={() => setModalIsOpenOne(false)}
+        idCourse={idCourse}
       ></AddClass>
 
       <EditClass
         isOpen={modalIsOpenTwo}
         onClose={() => setModalIsOpenTwo(false)}
+        classCourse={selectedClass}
       ></EditClass>
+
+      <DeleteCourse
+        isOpen={modalIsOpenThree}
+        onClose={() => setModalIsOpenThree(false)}
+        classId={selectedClassId}
+        refreshCourses={getClassMenu}
+      />
     </>
   );
 }
