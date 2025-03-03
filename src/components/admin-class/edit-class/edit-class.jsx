1d3a7aa5b71bd63fc05db282663c0917cda7b02e
Deymer Ospina <deymer.ospina@gmail.com>
2025-03-03 06:09:59 -0500

    Endpoint consumption to edit a class in add-class

--- src/components/admin-class/edit-class/edit-class.jsx
+++ src/components/admin-class/edit-class/edit-class.jsx
@@ -1,19 +1,68 @@
 /* eslint-disable react/prop-types */
-// import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext, useState } from "react";
 // import Example from "../../assets/img/example-img.png";
-// import AppContext from "@context/app/app-context";
+import AppContext from "@context/app/app-context";
 // import { Link } from "react-router-dom";
 import Modal from "react-modal";
-// import axios from "axios";
+import axios from "axios";
 import "./edit-class.css";
 
-export default function EditClass({ isOpen, onClose }) {
+export default function EditClass({ isOpen, onClose, classCourse }) {
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const context = useContext(AppContext);
+  const urlApi = context.urlApi;
+  const apiKey = context.apiKey;
+
+  console.log("classCourse: ", classCourse);
+
+  useEffect(() => {
+    if (classCourse) {
+      setTitle(classCourse.class_title || "");
+      setDescription(classCourse.class_description || "");
+    }
+  }, [classCourse]);
+
+  async function handleUpdateClass() {
+    debugger
+    if (!classCourse || !classCourse.class_id) {
+      console.error("No hay un curso válido para actualizar");
+      return;
+    }
+
+    try {
+      const response = await axios.put(
+        `${urlApi}academy/u/update-class/${classCourse.class_id}`,
+        {
+          class_title: title,
+          class_description: description,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "api-key": apiKey,
+          },
+        }
+      );
+
+      if (response.data.success) {
+        console.log("Curso actualizado con éxito:", response.data);
+        onClose();
+      } else {
+        console.error("Error al actualizar el curso:", response.data.message);
+      }
+    } catch (error) {
+      console.error("Error en la solicitud de actualización:", error);
+    }
+  }
+
   return (
     <>
       <Modal
         isOpen={isOpen}
         onRequestClose={onClose}
-        contentLabel="Agregar Nuevo Curso"
+        contentLabel="Editar Curso"
         style={{
           overlay: { backgroundColor: "rgba(0,0,0,0.5)" },
           content: {
@@ -41,6 +90,8 @@ export default function EditClass({ isOpen, onClose }) {
             type="text"
             className="input__edit-class lg-margin-bottom"
             placeholder="Enter course title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
           <label
             htmlFor="course-description-edit"
@@ -53,10 +104,15 @@ export default function EditClass({ isOpen, onClose }) {
             type="text"
             className="textarea__edit-class lg-margin-bottom"
             placeholder="Enter course description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           ></textarea>
 
-          <button className="btn-edit__edit-class lg-margin-bottom">
-            Save Course
+          <button
+            className="btn-edit__edit-class lg-margin-bottom"
+            onClick={handleUpdateClass}
+          >
+            Save Class
           </button>
 
           <button onClick={onClose} className="btn-back__edit-class">
