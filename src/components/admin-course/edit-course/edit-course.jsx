a81a0c67b76e4a8cbd61de62ad965de59590b1b1
Deymer Ospina <deymer.ospina@gmail.com>
2025-03-17 22:26:26 -0500

    Reconfiguration of backend consumption for administration courses and design adjustments

--- src/components/admin-course/edit-course/edit-course.jsx
+++ src/components/admin-course/edit-course/edit-course.jsx
@@ -7,9 +7,18 @@ import { useEffect, useState, useContext } from "react";
 import Modal from "react-modal";
 import "./edit-course.css";
 
-export default function EditCourse({ isOpen, onClose, course }) {
+export default function EditCourse({
+  isOpen,
+  onClose,
+  course,
+  refreshCourses,
+}) {
   const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
   const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const context = useContext(AppContext);
   const urlApi = context.urlApi;
   const apiKey = context.apiKey;
@@ -17,21 +26,27 @@ export default function EditCourse({ isOpen, onClose, course }) {
   useEffect(() => {
     if (course) {
       setTitle(course.course_title || "");
+      setImage(course.main_photo.bg_photo || "");
       setDescription(course.description_course || "");
     }
   }, [course]);
 
   async function handleUpdateCourse() {
     if (!course || !course.id) {
+      setError("Los campos no pueden estar vacíos");
       console.error("No hay un curso válido para actualizar");
       return;
     }
+
+    setLoading(true);
+    setError("");
 
     try {
       const response = await axios.put(
         `${urlApi}academy/u/update-course/${course.id}`,
         {
           course_title: title,
+          main_photo: image,
           description_course: description,
         },
         {
@@ -49,7 +64,12 @@ export default function EditCourse({ isOpen, onClose, course }) {
         console.error("Error al actualizar el curso:", response.data.message);
       }
     } catch (error) {
+      setError("Hubo un problema al actualizar el curso");
       console.error("Error en la solicitud de actualización:", error);
+    } finally {
+      setLoading(false);
+      refreshCourses();
+      onClose();
     }
   }
 
@@ -104,11 +124,43 @@ export default function EditCourse({ isOpen, onClose, course }) {
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
 
+          {/* Borrar según lo requerido */}
+
+          <label
+            htmlFor="course-img-edit"
+            className="label__edit-course sm-margin-bottom"
+          >
+            Course Url Img
+          </label>
+          <input
+            id="course-img-edit"
+            type="text"
+            className="input__edit-course lg-margin-bottom"
+            placeholder="Enter course title"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+
+          {/* Quitar el disabled en caso de eliminar el anterior */}
+
+          <label className="label__add-course sm-margin-bottom" htmlFor="">
+            Image Upload
+          </label>
+          <div className="cntr-input__add-course lg-margin-bottom">
+            <input className="file__add-course" type="file" disabled />
+            <button className="btn-upload__add-course" disabled>
+              ⬆
+            </button>
+          </div>
+
+          {error && <p style={{ color: "red" }}>{error}</p>}
+
           <button
             className="btn-edit__edit-course lg-margin-bottom"
             onClick={handleUpdateCourse}
+            disabled={loading}
           >
-            Save Course
+            {loading ? "Editing..." : "Edit Course"}
           </button>
 
           <button onClick={onClose} className="btn-back__edit-course">
