0efef8c9c3c20c56b0f533de6ff54a8ba18cea04
Deymer Ospina <deymer.ospina@gmail.com>
2025-03-30 03:46:15 -0500

    Modifying endpoint usage and preparing for uploading and editing files at  class

--- src/components/academy/admin-class/add-class/add-class.jsx
+++ src/components/academy/admin-class/add-class/add-class.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-import { useContext, useState } from "react";
-// import Example from "../../assets/img/example-img.png";
+import { useContext, useState, useCallback } from "react";
+import { Upload, Trash2 } from "lucide-react";
+import { useDropzone } from "react-dropzone";
 import AppContext from "@context/app/app-context";
 import Modal from "react-modal";
 import axios from "axios";
 import "./add-class.css";
-// import { DatabaseBackup } from "lucide-react";
 
 export default function AddClass({
   isOpen,
@@ -23,7 +23,42 @@ export default function AddClass({
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
+  // Nueva subida de image
+  const [imageOpen, setImageOpen] = useState(false);
+  const [imageUpload, setImageUpload] = useState("");
+  const [formImageUpload, setFormImageUpload] = useState("");
+  const [files, setFiles] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    if (acceptedFiles.length > 0) {
+      setFiles(acceptedFiles);
+      setImageUpload(URL.createObjectURL(acceptedFiles[0]));
+      setFormImageUpload(acceptedFiles[0]);
+      setError("");
+    }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: "image/jpeg, image/png, image/webp, video/mp4",
+  });
+
+  function cancelUploadImage() {
+    setImageOpen(false);
+    setFiles([]);
+    setImageUpload("");
+  }
+
+  // --------------------------------------
+
   async function handleAddClass() {
+    if (imageOpen && imageUpload.length == 0) {
+      setError("Por favor, suba una imagen");
+      return;
+    } else {
+      setError("");
+    }
+
     if (
       !classTitle.trim() ||
       !classDescription.trim() ||
@@ -37,17 +72,25 @@ export default function AddClass({
     setError("");
 
     try {
-      const response = await axios.post(
-        `${urlApi}academy/i/add-class`,
-        {
+      const formData = new FormData();
+      formData.append("file", formImageUpload);
+      formData.append("page", "academy");
+      formData.append(
+        "data",
+        JSON.stringify({
           id_course: idCourse,
           class_title: classTitle,
           class_description: classDescription,
           class_content: classContent,
-        },
+        })
+      );
+
+      const response = await axios.post(
+        `${urlApi}academy/i/add-class`,
+        formData,
         {
           headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "multipart/form-data",
             "api-key": apiKey,
           },
         }
@@ -57,6 +100,8 @@ export default function AddClass({
         alert("Clase agregado con éxito");
         setClassTitle("");
         setClassDescription("");
+        setImageUpload("");
+        setFormImageUpload("");
         setClassContent("");
       } else {
         throw new Error("Error al agregar la clase");
@@ -88,6 +133,7 @@ export default function AddClass({
             backgroundColor: "white",
             display: "flex",
             flexDirection: "column",
+            overflowY: "scroll",
           },
         }}
       >
@@ -118,31 +164,83 @@ export default function AddClass({
             required
           ></textarea>
 
-          {/* Borrar según lo requerido */}
-
-          <label className="label__add-class sm-margin-bottom" htmlFor="">
-            Class Title
-          </label>
-          <input
-            className="input__add-class sm-margin-bottom"
-            type="text"
-            placeholder="Enter course title"
-            value={classContent}
-            onChange={(e) => setClassContent(e.target.value)}
-            required
-          />
-
-          {/* Quitar el disabled en caso de eliminar el anterior */}
-
           <label className="label__add-class sm-margin-bottom" htmlFor="">
             Image Upload
           </label>
-          <div className="cntr-input__add-class lg-margin-bottom">
-            <input className="file__add-class" type="file" disabled />
-            <button className="btn-upload__add-class" disabled>
-              ⬆
-            </button>
-          </div>
+          {!imageOpen && (
+            <>
+              <div className="cntr-input__add-course lg-margin-bottom">
+                <button
+                  onClick={() => setImageOpen(true)}
+                  className="btn-upload__add-course"
+                >
+                  Cambiar imagen
+                </button>
+              </div>
+            </>
+          )}
+
+          {imageOpen && (
+            <section className="upload-section">
+              <h1 className="title__add-class sm-margin-bottom">
+                Añadir nueva imagen
+              </h1>
+              <br />
+
+              {files.length === 0 ? (
+                <div
+                  {...getRootProps()}
+                  className={`w-full max-w-md p-8 rounded-lg border-2 border-dashed transition-colors ${
+                    isDragActive ? "border-neutral-400" : "border-neutral-600"
+                  }`}
+                >
+                  <input {...getInputProps()} />
+                  <div className="flex flex-col items-center text-center">
+                    <Upload className="w-12 h-12 mb-4 text-neutral-400" />
+                    <p className="mb-2 text-lg font-medium text-neutral-300">
+                      {isDragActive
+                        ? "Suelta los archivos aquí"
+                        : "Arrastre y suelte archivos aquí"}
+                    </p>
+                    <p className="mb-4 text-sm text-neutral-500">or</p>
+                    <button className="px-4 py-2 text-sm font-medium text-neutral-200 bg-neutral-800 rounded-md hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-neutral-600">
+                      Select Files
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div className="mt-4 flex flex-col items-center">
+                  {imageUpload && (
+                    <img
+                      src={imageUpload}
+                      alt="Preview"
+                      className="w-full h-50 max-h-52 object-cover rounded-md mb-4"
+                    />
+                  )}
+                  <button
+                    onClick={() => {
+                      setFiles([]);
+                      setImageUpload("");
+                    }}
+                    className="px-4 py-2 text-sm font-medium text-red-600 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-red-400 flex items-center"
+                  >
+                    <Trash2 className="w-4 h-4 mr-2" /> Eliminar archivo
+                  </button>
+                </div>
+              )}
+
+              {error && <p style={{ color: "red" }}>{error}</p>}
+              <br />
+              <div className="flex justify-center mt-4 items-center gap-8">
+                <button
+                  onClick={() => cancelUploadImage()}
+                  className="btn-back__edit-course"
+                >
+                  Cancelar
+                </button>
+              </div>
+            </section>
+          )}
 
           {error && <p style={{ color: "red" }}>{error}</p>}
 
