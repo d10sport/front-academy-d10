bc379eca70a53b5128206c6a405efe0be2f01b30
Deymer Ospina <deymer.ospina@gmail.com>
2025-04-02 17:11:19 -0500

    Loader implemented

--- src/routes/admin/admin.jsx
+++ src/routes/admin/admin.jsx
@@ -3,22 +3,38 @@ import AboutUsAdmin from "@components/admin/admin-aboutus/admin-aboutus.jsx";
 import GalleryAdmin from "@components/admin/admin-gallery/admin-gallery.jsx";
 import HomeAdmin from "@components/admin/admin-home/admin-home.jsx";
 import NewsAdmin from "@components/admin/admin-news/admin-news.jsx";
+import Loader from "../../ui/loaders/fake-load/loader.fake.jsx";
+import { useState, useEffect } from "react";
 import "./admin.css";
 
 export default function Admin() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <section className="admin-section">
-        <HomeAdmin />
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className="admin-section">
+          <HomeAdmin />
 
-        <AboutUsAdmin />
+          <AboutUsAdmin />
 
-        <ServicesAdmin />
+          <ServicesAdmin />
 
-        <GalleryAdmin />
+          <GalleryAdmin />
 
-        <NewsAdmin />
-      </section>
+          <NewsAdmin />
+        </section>
+      )}
     </>
   );
 }
