3f604f99b390b7acf6e6ec17354424fa8a08a5f6
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-05-11 22:03:50 -0500

    Instagram user

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -21,6 +21,7 @@ export default function Header() {
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   const getPermissions = useCallback(async () => {
+    if(permissionsSystem.length > 0) return;
     let userPermissions = permissionsUser;
     if (userPermissions?.length === 0 || userPermissions == undefined) {
       userPermissions = await fetchPermissionsUser();
@@ -55,12 +56,13 @@ export default function Header() {
     fetchPermissionsUser,
     fetchPermissionsRoles,
     fetchUser,
+    user,
     user?.id_role,
   ]);
 
   useEffect(() => {
     getPermissions();
-  }, [user, isVisible, permissionsUser]);
+  }, [user, isVisible]);
 
   if (!user || Object.keys(user).length === 0) {
     return <div className="relative bg-transparent top-0 left-0"></div>;
