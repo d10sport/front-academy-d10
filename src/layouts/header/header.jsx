847ed3d57b038874e2d463fa46ea5e36d984e8b7
Deymer Ospina <deymer.ospina@gmail.com>
2025-05-22 03:38:11 -0500

    Header redesign

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -1,10 +1,10 @@
-import MenuNav from "./menu-nav/menu-nav.jsx";
 import UserInfo from "./user-info/user-info.jsx";
 import { useContext, useEffect, useState, useCallback, useMemo } from "react";
 import Example from "../../assets/img/example-img.png";
 import { LogoHeader } from "../../utils/icons/icons";
 import AppContext from "@context/app/app-context";
 import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react";
 import { toast } from "sonner";
 import "./header.css";
 
@@ -20,21 +20,12 @@ export default function Header() {
   const [permissionsSystem, setPermissionsSystem] = useState([]);
   const [isVisible, setIsVisible] = useState(false);
 
-  const [modalIsOpenOne, setModalIsOpenOne] = useState(false);
   const [modalIsOpenTwo, setModalIsOpenTwo] = useState(false);
 
-  const [deviceType, setDeviceType] = useState("desktop");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
-  const changeBtnMenu = useMemo(() => {
-    switch (deviceType) {
-      case "mobile":
-        return { show: true };
-      case "tablet":
-        return { show: true };
-      default:
-        return { show: false };
-    }
-  }, [deviceType]);
+  const [deviceType, setDeviceType] = useState("desktop");
 
   const changeBtnInfoUser = useMemo(() => {
     switch (deviceType) {
@@ -70,7 +61,7 @@ export default function Header() {
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   const getPermissions = useCallback(async () => {
-    if(permissionsSystem.length > 0) return;
+    if (permissionsSystem.length > 0) return;
     let userPermissions = permissionsUser;
     if (userPermissions?.length === 0 || userPermissions == undefined) {
       userPermissions = await fetchPermissionsUser();
@@ -110,6 +101,20 @@ export default function Header() {
   ]);
 
   useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolled]);
+
+  useEffect(() => {
     getPermissions();
   }, [user, isVisible]);
 
@@ -118,117 +123,180 @@ export default function Header() {
   }
 
   return (
-    <nav id="nav_header" className="nav">
-      <LogoHeader />
-
-      {changeBtnMenu.show ? (
-        <h1
-          className="title-btn__class"
-          style={{ width: "fit-content", padding: "0px 10px" }}
-          onClick={() => setModalIsOpenOne(true)}
+    <>
+      <nav className="fixed top-4 left-0 right-0 z-50 mx-auto w-[95%] max-w-7xl">
+        <div
+          className={`mx-auto flex h-15 items-center justify-between rounded-full px-6 shadow-lg backdrop-blur-sm transition-all duration-300 ${
+            scrolled ? "bg-black/40" : "bg-black/80"
+          }`}
         >
-          Menu
-        </h1>
-      ) : (
-        <ul className="list__nav">
-          {permissionsSystem?.length > 0 &&
-            permissionsSystem.map((permission) => (
-              <li
-                key={permission.permission_id ?? permission.id_permission}
-                className="item__nav"
-              >
-                <Link to={permission.link}>
-                  {permission.description_permission}
-                </Link>
-              </li>
-            ))}
-        </ul>
-      )}
-
-      {changeBtnInfoUser.show ? (
-        <button
-          className="button__button-nav"
-          onClick={() => setModalIsOpenTwo(true)}
-        >
-          <p>{user?.first_names ?? user?.president}</p>
-          &nbsp; &nbsp;
-          <div className="cntr-img__button-nav">
-            <img src={Example} alt="img" className="img__button-nav" />
-          </div>
-        </button>
-      ) : (
-        <button className="button__button-nav" onClick={toggleVisibility}>
-          <p>{user?.first_names ?? user?.president}</p>
-          &nbsp; &nbsp;
-          <div className="cntr-img__button-nav">
-            <img src={Example} alt="img" className="img__button-nav" />
-          </div>
-        </button>
-      )}
-
-      <div className={`info-user ${isVisible ? "visible" : ""}`}>
-        <div className="cntr-one-item__info-user">
-          <div className="cntr-img__button-nav">
-            <img src={Example} alt="img" className="img__info-user" />
-          </div>
-          <div className="items__info-user">
-            <p className="text__info-user">
-              {user?.first_names ?? user?.name_club} {user?.last_names ?? ""}
-            </p>
-          </div>
-        </div>
-
-        <div className="cntr-two-item__info-user">
-          <h1 className="title__info-user">Información de usuario</h1>
-          <p className="text__info-user">
-            <b>Nombre: </b> {user?.first_names ?? user?.president}{" "}
-            {user?.last_names ?? ""}
-          </p>
-          <p className="text__info-user">
-            <b>Email: </b> {user?.email}
-          </p>
-          <p className="text__info-user">
-            <b>Rol: </b>{" "}
-            {user?.role.charAt(0).toUpperCase() + user?.role.slice(1)}
-          </p>
-          <p className="text__info-user">
-            <b>Club: </b> {user?.club?.name_club ?? user?.name_club}
-          </p>
-          <Link
-            to={`/change-pass`}
-            className="text__info-user"
-          >
-            Cambiar contraseña
+          <Link className="select-none text-xl font-bold" to={"/"}>
+            <LogoHeader />
+            {/* {data.logo == "" ? (
+              <ImageLogo style={{ maxWidth: "50px" }} alt="Logo" />
+            ) : (
+              <img
+                src={data.logo}
+                alt="logo D10"
+                className="logo max-w-[50px]"
+              />
+            )} */}
           </Link>
-        </div>
+          <div className="hidden space-x-8 md:flex">
+            {permissionsSystem?.length > 0 &&
+              permissionsSystem.map((permission) => (
+                <span
+                  key={permission.permission_id ?? permission.id_permission}
+                  className="text-sm uppercase tracking-wider hover:text-gray-300"
+                >
+                  <Link to={permission.link}>
+                    {permission.description_permission}
+                  </Link>
+                </span>
+              ))}
+          </div>
 
-        <div className="cntr-three-item__info-user">
+          {/* Start btn menu */}
+
           <button
-            onClick={() => {
-              toggleVisibility();
-              closeSession();
-              toast.success("Sesión cerrada correctamente");
-            }}
-            className="button__info-user"
+            className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input hover:bg-accent hover:text-accent-foreground h-10 w-10 md:hidden"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
-            Log out
+            <span className="sr-only">
+              {isMenuOpen ? "Close menu" : "Open menu"}
+            </span>
+            {isMenuOpen ? (
+              <X className="h-4 w-4" />
+            ) : (
+              <Menu className="h-4 w-4" />
+            )}
           </button>
+
+          {/* End btn menu */}
+
+          {/* Start btn user info */}
+
+          {changeBtnInfoUser.show ? (
+            <button
+              className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input hover:bg-accent hover:text-accent-foreground h-10 w-fit md:hidden"
+              onClick={() => setModalIsOpenTwo(true)}
+            >
+              <div className="w-fit h-full">
+                <img
+                  src={Example}
+                  alt="img"
+                  className="w-full h-full rounded"
+                />
+              </div>
+              &nbsp;
+              <p>{user?.first_names ?? user?.president}</p>
+            </button>
+          ) : (
+            <button
+              className="justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background
+            transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
+            focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none
+            [&_svg]:size-4 [&_svg]:shrink-0 border h-9 rounded-md px-3 hidden md:flex items-center border-white/20
+            hover:bg-white hover:text-black"
+              onClick={toggleVisibility}
+            >
+              <div className="w-fit h-full">
+                <img
+                  src={Example}
+                  alt="img"
+                  className="w-full h-full rounded"
+                />
+              </div>
+              &nbsp;
+              <p>{user?.first_names ?? user?.president}</p>
+            </button>
+          )}
+
+          {/* End btn user info */}
+
+          {/* Start user info */}
+
+          <div className={`info-user ${isVisible ? "visible" : ""}`}>
+            <div className="cntr-one-item__info-user">
+              <div className="cntr-img__button-nav">
+                <img src={Example} alt="img" className="img__info-user" />
+              </div>
+              <div className="items__info-user">
+                <p className="text__info-user">
+                  {user?.first_names ?? user?.name_club}{" "}
+                  {user?.last_names ?? ""}
+                </p>
+              </div>
+            </div>
+
+            <div className="cntr-two-item__info-user">
+              <h1 className="title__info-user">Información de usuario</h1>
+              <p className="text__info-user">
+                <b>Nombre: </b> {user?.first_names ?? user?.president}{" "}
+                {user?.last_names ?? ""}
+              </p>
+              <p className="text__info-user">
+                <b>Email: </b> {user?.email}
+              </p>
+              <p className="text__info-user">
+                <b>Rol: </b>{" "}
+                {user?.role.charAt(0).toUpperCase() + user?.role.slice(1)}
+              </p>
+              <p className="text__info-user">
+                <b>Club: </b> {user?.club?.name_club ?? user?.name_club}
+              </p>
+              <Link to={`/change-pass`} className="text__info-user">
+                Cambiar contraseña
+              </Link>
+            </div>
+
+            <div className="cntr-three-item__info-user">
+              <button
+                onClick={() => {
+                  toggleVisibility();
+                  closeSession();
+                  toast.success("Sesión cerrada correctamente");
+                }}
+                className="button__info-user"
+              >
+                Log out
+              </button>
+            </div>
+          </div>
+
+          {/* End user info */}
         </div>
-      </div>
 
-      <MenuNav
-        isOpen={modalIsOpenOne}
-        onClose={() => setModalIsOpenOne(false)}
-        permission_system={permissionsSystem}
-      ></MenuNav>
+        <UserInfo
+          isOpen={modalIsOpenTwo}
+          onClose={() => setModalIsOpenTwo(false)}
+          imgExample={Example}
+          userInfo={user}
+          closeUserInfo={closeSession}
+        ></UserInfo>
+      </nav>
 
-      <UserInfo
-        isOpen={modalIsOpenTwo}
-        onClose={() => setModalIsOpenTwo(false)}
-        imgExample={Example}
-        userInfo={user}
-        closeUserInfo={closeSession}
-      ></UserInfo>
-    </nav>
+      {/* Start menu info */}
+
+      {isMenuOpen && (
+        <div className="fixed inset-0 z-40 flex flex-col bg-black pt-16 md:hidden">
+          <div className="flex flex-col space-y-4 p-4">
+            {permissionsSystem?.length > 0 &&
+              permissionsSystem.map((permission) => (
+                <span
+                  key={permission.permission_id ?? permission.id_permission}
+                  className="border-b border-zinc-800 py-3 text-lg font-medium"
+                >
+                  <Link to={permission.link}>
+                    {permission.description_permission}
+                  </Link>
+                </span>
+              ))}
+          </div>
+        </div>
+      )}
+
+      {/* End menu info */}
+    </>
   );
 }
